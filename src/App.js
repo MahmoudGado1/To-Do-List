5be919d4e6884e7e176988daac60bdb7f554b6ce
MahmoudGado1 <mahmoudgado444@gmail.com>
2024-06-14 17:26:32 +0300

    to do list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import { useRef, useState } from 'react';
 import './App.css';
+
 function App() {
-  const [x , setx]= useState ([])
+
+  const [x , setx] = useState([])
+
   const inputRef = useRef()
-  const add = () =>{
-    const value = inputRef.current.value
-    // console.log(value)
-    setx([...x,value])
-    inputRef.current.value=""
+
+  const add = () => {
+      const value = inputRef.current.value
+      const newData = {completed : false , value} 
+      setx([...x , newData])
+
+      inputRef.current.value = ""
   }
+
+  const itemDone = (index) =>{
+     const newx = [...x]
+
+     newx[index].completed =  !newx[index].completed
+
+     setx(newx)
+  }
+
+  
+  const toDelete = (index) => {
+     const newx = [...x]
+
+     newx.splice(index, 1)
+
+     setx(newx)
+  }
+
   return (
     <div className="App">
-      <h2>to do list</h2>
 
-      <ul>
-        {
-          x.map((item)=>{
-            return <li>{item}</li>
-          })
-        }
-      </ul>
+         <h2>To do List</h2>
+          <ul>
+               {
+                  x.map(({value , completed} , index ) => {
+                    return <div className='div10'> 
+                      <li className={completed ? "diffstyle" : ""} onClick={ () => itemDone(index)} >{value}</li>
 
-      <input ref={inputRef} placeholder='enter new task...'></input>
+                    <span onClick={() => toDelete(index)} >X</span>
+                    </div>
+                  })
+               }
+          </ul>
 
-      <button onClick={add}>Add</button>
+          <input  ref={inputRef}  placeholder='Enter new task...' />
+
+          <button onClick={add} >Add</button>
+
     </div>
   );
 }
